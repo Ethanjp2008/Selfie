ee239611971fd4da5d4e24fc70626241fafb2a06
Hurricane <87663837+Ethanjp2008@users.noreply.github.com>
2022-01-07 20:39:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+var recognition = new SpeechRecognition;
+
+function start() {
+    document.getElementById("textbox").innerHTML = "";
+    recognition.start();
+}
+recognition.onresult = function run(event){
+    console.log(event);
+    var Content = event.results[0][0].transcript;
+    console.log(Content);
+    document.getElementById("textbox").innerHTML = Content;
+    speak()
+}
+
+function speak() {
+    var synth = window.SpeechSynthesis;
+    speek_data = document.getElementById("textbox").value;
+    var utterThis = new SpeechSynthesisUtterance(speechSynthesis);
+    synth.speak(utterThis);
+    webcam.attach(camera);
+}
+
+Webcam.set({
+    width:360,
+    height:250,
+    image_format:'png',
+    png_quality:90
+});
+
+camera = document.getElementById("camera");
+
